debf2e31638c55a5a45827bed3c6cb1b32b40a33
ces-p <cesarportillord@gmail.com>
2021-06-23 19:16:47 -0400

    first commit

---
+++ qsort.hs
@@ -0,0 +1,20 @@
+main::IO()
+main = undefined
+
+quickSort :: [Int] -> [Int]
+quickSort [] = []
+quickSort list = quickSort left_sublist
+               ++ pivot_list
+               ++ right_sublist
+    where
+        pivot :: Int
+        pivot = head list
+
+        left_sublist :: [Int]
+        left_sublist = filter (<pivot) list
+
+        pivot_list :: [Int]
+        pivot_list = filter (==pivot) list
+
+        right_sublist :: [Int]
+        right_sublist = filter (>pivot) list
